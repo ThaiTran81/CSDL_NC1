71f663a41f3572a00aca3fa807c08902aeb410ae
huynhkha2601 <63405972+huynhkha2601@users.noreply.github.com>
2021-10-20 15:43:43 +0700

    Trigger tinh thanh tien
    Tính thành tiền CT_HoaDon

--- QLHD.sql
+++ QLHD.sql
@@ -22,15 +22,53 @@ create table KhachHang(
 	primary key(MaKH)
 )
 
-CREATE TABLE HoaDon(	MaHD VARCHAR(15),	MaKH VARCHAR(15),	NgayLap DATE,	TongTien MONEY,		PRIMARY KEY (MaHD))
+CREATE TABLE HoaDon
+(
+	MaHD VARCHAR(15),
+	MaKH VARCHAR(15),
+	NgayLap DATE,
+	TongTien MONEY,	
+	PRIMARY KEY (MaHD)
+)
 
-CREATE TABLE CT_HoaDon(  MaHD VARCHAR(15),  MaSP VARCHAR(15),  SoLuongTon INT,  GiaBan MONEY NOT NULL,  GiaMua MONEY NOT NULL,  ThanhTien MONEY,  CONSTRAINT PK_CTHOADON PRIMARY KEY (MaHD, MaSP))
+CREATE TABLE CT_HoaDon(
+  MaHD VARCHAR(15),
+  MaSP VARCHAR(15),
+  SoLuong INT,
+  GiaBan MONEY NOT NULL,
+  GiaGiam MONEY NOT NULL,
+  ThanhTien MONEY,
+  CONSTRAINT PK_CTHOADON PRIMARY KEY (MaHD, MaSP)
+)
 
 
-create table SanPham(	MaSP varchar(15),	TenSP nvarchar(20),	SoLuongTon int,	Mota nvarchar(100),	Gia money,	primary key (MaSP))
+create table SanPham
+(
+	MaSP varchar(15),
+	TenSP nvarchar(20),
+	SoLuongTon int,
+	Mota nvarchar(100),
+	Gia money,
+	primary key (MaSP)
+)
 
-ALTER TABLE HoaDonADD CONSTRAINT FK_HoaDon_KhachHangFOREIGN KEY (MaKH)REFERENCES KhachHang(MaKH)
+ALTER TABLE HoaDon
+ADD CONSTRAINT FK_HoaDon_KhachHang
+FOREIGN KEY (MaKH)
+REFERENCES KhachHang(MaKH)
 
 
 alter table CT_HoaDon add
-CONSTRAINT FK_CTHoaDon_HoaDon FOREIGN KEY (MaHD) REFERENCES HoaDon(MaHD),CONSTRAINT FK_CTHoaDon_SanPham FOREIGN KEY (MaSP) REFERENCES SanPham(MaSP)  
+CONSTRAINT FK_CTHoaDon_HoaDon FOREIGN KEY (MaHD) REFERENCES HoaDon(MaHD),
+CONSTRAINT FK_CTHoaDon_SanPham FOREIGN KEY (MaSP) REFERENCES SanPham(MaSP)  
+
+go
+create trigger TinhThanhTien on CT_HoaDon 
+after insert, update
+as 
+begin
+	update CT_HoaDon
+	set CT_HoaDon.ThanhTien = (CT_HoaDon.GiaBan-CT_HoaDon.GiaGiam)*CT_HoaDon.SoLuong
+	FROM CT_HoaDon
+    INNER JOIN inserted i on i.MaHD = CT_HoaDon.MaHD and i.MaSP=CT_HoaDon.MaSP
+end
---
+++ Trigger.sql
@@ -0,0 +1,13 @@
+﻿USE QLHD
+GO
+
+create trigger TinhThanhTien on CT_HoaDon 
+after insert, update
+as 
+begin
+	update CT_HoaDon
+	set CT_HoaDon.ThanhTien = (CT_HoaDon.GiaBan-CT_HoaDon.GiaGiam)*CT_HoaDon.SoLuong
+	FROM CT_HoaDon
+    INNER JOIN inserted i on i.MaHD = CT_HoaDon.MaHD and i.MaSP=CT_HoaDon.MaSP
+END
+GO	
