4986bb8095c96ce9e9d6b08c15e28c34fc8ef6c7
khiemlethanh <63852647+khiemlethanh@users.noreply.github.com>
2021-10-20 16:48:29 +0700

    add trigger tinhtongtien
    Tính Tổng tiền

--- Trigger.sql
+++ Trigger.sql
@@ -10,4 +10,19 @@ begin
 	FROM CT_HoaDon
     INNER JOIN inserted i on i.MaHD = CT_HoaDon.MaHD and i.MaSP=CT_HoaDon.MaSP
 END
-GO	
+GO
+
+CREATE TRIGGER TinhTongTien ON dbo.CT_HoaDon
+FOR	INSERT,UPDATE, DELETE AS 
+BEGIN
+	UPDATE dbo.HoaDon
+	SET dbo.HoaDon.TongTien = (
+		SELECT SUM(ThanhTien)
+		FROM dbo.CT_HoaDon
+		WHERE dbo.CT_HoaDon.MaHD = i.MaHD
+	)
+	FROM Inserted i 
+	JOIN dbo.HoaDon ON HoaDon.MaHD = i.MaHD 
+END
+GO
+	
