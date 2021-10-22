c1f397c452fbe85a4049cad2a4173e6f78dfe744
Trần Hoàng Thái <63252276+ThaiTran81@users.noreply.github.com>
2021-10-22 14:56:46 +0700

    D, E aswered

---
+++ Query.sql
@@ -0,0 +1,24 @@
+﻿
+--D
+SELECT
+  sp.MaSP
+ ,sp.TenSP
+ ,sp.SoLuongTon
+FROM SanPham sp
+WHERE sp.SoLuongTon < 100
+
+
+--E
+SELECT
+  sp.MaSP
+ ,sp.TenSP
+ ,SUM(ct.SoLuong)
+FROM CT_HoaDon ct
+JOIN SanPham sp
+  ON ct.MaSP = sp.MaSP
+GROUP BY sp.MaSP
+        ,sp.TenSP
+HAVING SUM(ct.SoLuong) >= ALL (SELECT
+    SUM(ct1.SoLuong)
+  FROM CT_HoaDon ct1
+  GROUP BY ct1.MaSP)
